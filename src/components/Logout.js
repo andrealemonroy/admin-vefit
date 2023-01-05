c1d0a5372306e5112c87826ee8e51b977f32ba01
fiorellasaro <fiorellasaro27@gmail.com>
2023-01-05 00:06:55 -0500

    login and logout config with outh0

---
+++ src/components/Logout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Logout = () => {
+  const { logout } = useAuth0();
+  return (
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onClick={() => logout()}
+    >
+      Logout
+    </button>
+  );
+};
+
+export default Logout;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Auth0Provider } from "@auth0/auth0-react";
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
